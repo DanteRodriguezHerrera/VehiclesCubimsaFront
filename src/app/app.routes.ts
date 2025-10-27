5d09d7a2147b0154bc9b5d2256e31c7e78bad566
Dante Rodriguez <papikra222@gmail.com>
2025-10-26 22:10:20 -0600

    Initialize the project, configure first routes and first design.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
+import { VehiclesComponent } from './components/vehicles/vehicles.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        redirectTo: '/vehicles',
+        pathMatch: 'full'
+    },
+    {
+        path: 'vehicles',
+        component: VehiclesComponent
+    },
+    {
+        path: '**',
+        component: NotFoundComponent
+    }
+];
